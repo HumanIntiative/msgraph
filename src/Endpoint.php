32b0aadefd675f3db24e1dfb787b4c294f7ab2ef
Zein Miftah <709962+zmiftah@users.noreply.github.com>
2023-01-10 11:04:54 +0700

    prepare for v0.2.0

--- src/Endpoint.php
+++ src/Endpoint.php
@@ -4,14 +4,14 @@ namespace pkpudev\graph;
 
 /**
  * Constant Endpoint to Ms Graph Api
- * 
+ *
  * @author Zein Miftah <zmiftahdev@gmail.com>
  * @license MIT
  */
 class Endpoint
 {
-  const BASE = 'https://login.microsoftonline.com';
-  const TOKEN = '/oauth2/v2.0/token';
-  const SCOPE = 'https://graph.microsoft.com/.default';
-  const GRANT = 'client_credentials';
+    const BASE = 'https://login.microsoftonline.com';
+    const TOKEN = '/oauth2/v2.0/token';
+    const SCOPE = 'https://graph.microsoft.com/.default';
+    const GRANT = 'client_credentials';
 }
